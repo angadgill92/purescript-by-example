0869514cce66a62986a9114e02919dd915e49f92
Angad Gill <angadpalsingh92@gmail.com>
2017-08-09 11:40:47 +0530

    Add Chapter 2 Exercise

--- chapter2/src/Main.purs
+++ chapter2/src/Main.purs
@@ -1,10 +1,25 @@
 module Main where
 
-import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
+import Control.Monad.Eff
 
-diagonal :: Number -> Number -> Number
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+import Math (sqrt, pi)
+import Prelude (Unit, (*), (+), discard)
+
+type Width  = Number
+type Height = Number
+type Radius = Number
+type Area   = Number
+
+diagonal :: Width -> Height -> Area
 diagonal w h = sqrt (w * w + h * h)
 
-main = logShow (diagonal 3.0 4.0)
+circleArea :: Radius -> Area
+circleArea r = pi * r * r
+
+
+main :: forall e. Eff (console :: CONSOLE| e) Unit
+main = do
+    logShow (diagonal 3.0 4.0)
+    logShow (circleArea 1.0)
+    
